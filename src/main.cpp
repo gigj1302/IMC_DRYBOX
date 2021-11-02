9a2cce0914dc8a268054b39cd75000fb75040e9c
gigj1302 <55798666+gigj1302@users.noreply.github.com>
2021-11-02 14:09:40 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -32,33 +32,6 @@ volatile bool boolreadSensor;
 
 int valueButton;
 int mode;
-/*
-int decodeButton(int value)
-{
-    int numberButton = 0;
-    if (value <= 60) {
-       //lcd.print (" Right");
-       numberButton = 5;
-    }
-    else if ((value <= 400)&&(value >= 61)){
-       //lcd.print ("    Up");
-       numberButton = 3;
-    }
-    else if ((value <= 600)&&(value >= 401)){
-       //lcd.print ("  Down");
-       numberButton = 4;
-    }
-    else if ((value <= 800)&&(value >= 601)){
-       //lcd.print ("  Left");
-       numberButton = 2;
-    }
-    else if ((value <= 1000)&&(value >= 801)){
-       //lcd.print ("Select");
-       numberButton = 1;
-    }
-    return numberButton;
-}
-*/
 
 void updateButton()
 {
@@ -80,6 +53,10 @@ void taskReadButton(void *pvParameters)
         vTaskDelay( 100 / portTICK_PERIOD_MS ); 
     }
 }
+
+
+
+
 
 void modeMenu(){
     lcd.setCursor(0, 0);
